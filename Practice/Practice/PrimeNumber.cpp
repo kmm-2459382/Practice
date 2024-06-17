41596457c7a56fb9c7e4d3e83e3c481a119423b7
kmm-2459382 <kmm2459382@stu.o-hara.ac.jp>
2024-06-17 12:02:27 +0900

    PrimeNumber_Improvement.cpp

--- Practice/Practice/PrimeNumber.cpp
+++ Practice/Practice/PrimeNumber.cpp
@@ -9,15 +9,19 @@ int main()
     const int N = 1000;
     unsigned long counter = 0;
 
-    for (int n = 2; n <= N; n++) {
+    for (int n = 2; n <= N; n++)
+    {
         int i;
-        for (i = 2; i < n; i++) {
+        for (i = 2; i < n; i++)
+        {
             counter++;
-            if (n % i == 0) {
+            if (n % i == 0)
+            {
                 break;
             }
         }
-        if (n == i) {
+        if (n == i)
+        {
             printf("%d ", n);
         }
     }
